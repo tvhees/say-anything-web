93b31a067a1320ce57f96801e7c8069ec1f639eb
tvhees <t.vanheeswijck@gmail.com>
2020-04-06 20:13:23 +0930

    BUGFIX: app server listening on heroku port

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ var app = express();
 var httpServer = http.Server(app);
 var io = socketIO(httpServer);
 
-var port = process.env.PORT;
-if (port == null || port == "") {
-  port = config.port;
-}
-
+var port = process.env.PORT || config.port;
 
 app.set('port', port);
 app.use('/static', express.static(__dirname + '/src/static'));
@@ -25,8 +21,8 @@ app.get('/', function(request, response) {
 });
 
 // Starts the server.
-httpServer.listen(5000, function() {
-  console.log('Starting server on port 5000');
+httpServer.listen(port, function() {
+  console.log('Starting server on port ' + port);
 });
 
 gameServer.initialise(io);
