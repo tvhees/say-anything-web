9675f08fe154ff1ce9f268f3ebbf2c3b1e946487
tvhees <t.vanheeswijck@gmail.com>
2020-04-08 21:13:07 +0930

    Fresh build

--- build/main.js
+++ build/main.js
@@ -14,7 +14,9 @@ var _server = _interopRequireDefault(require("./server"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// Dependencies
+// Configure app and start the server
+// Heroku requires the app to listen on a specific port
+// Fallback to local config when not running on Heroku
 var app = (0, _express["default"])();
 
 var httpServer = _http["default"].Server(app);
@@ -22,14 +24,12 @@ var httpServer = _http["default"].Server(app);
 var io = (0, _socket["default"])(httpServer);
 var port = process.env.PORT || _config["default"].port;
 app.set('port', port);
-app.use('/static', _express["default"]["static"](__dirname + '/static')); // Routing
-
+app.use('/static', _express["default"]["static"](__dirname + '/static'));
 app.get('/', function (request, response) {
-  response.sendFile(_path["default"].join(__dirname, '../index.html'));
-}); // Starts the server.
-
-httpServer.listen(port, function () {
-  console.log('Starting server on port ' + port);
+  return response.sendFile(_path["default"].join(__dirname, '../index.html'));
 });
+httpServer.listen(port, function () {
+  return console.log('Starting server on port ' + port);
+}); // Run the actual game with access to socket.io
 
 _server["default"].initialise(io);
